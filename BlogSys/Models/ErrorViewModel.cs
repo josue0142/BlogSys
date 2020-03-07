34d63b3e3dbe94ca087537bb302fea36ee581d10
Josue Jimenez <josuejjimenezd@gmail.com>
2020-03-07 09:32:49 -0400

    First Commit

---
+++ BlogSys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BlogSys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
